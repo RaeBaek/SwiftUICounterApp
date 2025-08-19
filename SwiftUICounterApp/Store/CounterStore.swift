fb25851968ad78b7745c078def9fc59bb8f291cb
RaeBaek <bmp117@naver.com>
2025-08-19 22:05:49 +0900

    feat: SwiftUI 카운터 앱 초기 구조 구성
    - 기존 ContentView.swift 이동
    - 상태 관리를 위한 CounterStore.swift 추가
    - 화면 구성을 위한 RootView.swift, SettingsView.swift 파일 생성

---
+++ SwiftUICounterApp/Store/CounterStore.swift
@@ -0,0 +1,8 @@
+//
+//  CounterStore.swift
+//  SwiftUICounterApp
+//
+//  Created by 백래훈 on 8/19/25.
+//
+
+import Foundation
---
+++ SwiftUICounterApp/Views/SettingsView.swift
@@ -0,0 +1,8 @@
+//
+//  SettingsView.swift
+//  SwiftUICounterApp
+//
+//  Created by 백래훈 on 8/19/25.
+//
+
+import Foundation
