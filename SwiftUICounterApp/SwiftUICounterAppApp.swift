2c8168cd21e9445653a776f8d50398492ccfbeea
RaeBaek <bmp117@naver.com>
2025-08-19 21:58:47 +0900

    SwiftUICounterApp first commit

---
+++ SwiftUICounterApp/SwiftUICounterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICounterAppApp.swift
+//  SwiftUICounterApp
+//
+//  Created by 백래훈 on 8/19/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICounterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
